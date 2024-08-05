75ddedd35637ac958781aa4fb66f7ccc3a7317a2
unfolks <araunay2@gmail.com>
2024-08-05 09:02:56 +0700

    void:2.2

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -1,22 +1,25 @@
 <template>
     <div>
-        <div class="flex justify-end mb-4">
-            <ModalAdd/>
+        <!-- <div class="flex justify-end mb-4">
+            <ModalAdd  />
+        </div> -->
+        <div>
+            <Table />
         </div>
-        <Table/>
-        
+
     </div>
 </template>
 
 <script>
-// import Table from '@/components/User/TableDataUser'
-// import ModalAdd from '@/components/User/UserAdd'
 import Table from '@/views/User/Components/TableUser.vue'
-import ModalAdd from '@/views/User/Components/AddUser.vue'
 export default {
     components: {
         Table,
-        ModalAdd
+    },
+    data() {
+        return {
+            
+        }
     }
 }
 </script>
